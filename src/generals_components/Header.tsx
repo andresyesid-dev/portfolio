8b7a735856a8f4799df4e3399b39d48e98ebe718
andresyesid-dev <andresyesid0505@gmail.com>
2024-11-27 17:12:07 -0500

    CU-86a5q7bat Redes sociales

--- src/generals_components/Header.tsx
+++ src/generals_components/Header.tsx
@@ -1,4 +1,7 @@
 import './styles/Header.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
@@ -14,6 +17,11 @@ function Header() {
                     continuous learning in software development. A good leader with a great ability 
                     to guide and support people in their growth. Creating the future, one line of code at a time.
                 </p>
+            </section>
+            <section className='social_media'>
+                <p><span><FontAwesomeIcon icon={faLinkedinIn} /></span> LinkedIn</p>
+                <p><span><FontAwesomeIcon icon={faGithub} /></span> GitHub</p>
+                <p><span className='solid'><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></span> Resume</p>
             </section>
         </>
     )
