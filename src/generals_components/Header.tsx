50a57cc0bfef3f18fd063e7c3c55d5c7c9f22540
andresyesid-dev <andresyesid0505@gmail.com>
2024-11-27 15:52:03 -0500

    CU-86a5qd1zr Descripción del encabezado

--- src/generals_components/Header.tsx
+++ src/generals_components/Header.tsx
@@ -7,6 +7,14 @@ function Header() {
                 <h1 className='im'>I'M</h1>
                 <h2 className='name'>Andres Motta</h2>
             </section>
+            <section className='description'>
+                <p>
+                    + 6 years of experience. <span> Senior Software Engineer</span> specializing in <span>DevOps, Full Stack 
+                    development for mobile, web, and desktop applications.</span> Friendly and passionate about 
+                    continuous learning in software development. A good leader with a great ability 
+                    to guide and support people in their growth. Creating the future, one line of code at a time.
+                </p>
+            </section>
         </>
     )
 }
