9b65b1f28c626e42adaa6047ad36beefe38828c7
andresyesid-dev <andresyesid0505@gmail.com>
2024-11-27 13:05:49 -0500

    CU-86a5q77c3 Títulos del encabezado

---
+++ src/generals_components/Header.tsx
@@ -0,0 +1,12 @@
+import './styles/Header.scss'
+
+function Header() {
+    return (
+        <section className='header'>
+            <h1 className='im'>I'M</h1>
+            <h2 className='name'>Andres Motta</h2>
+        </section>
+    )
+}
+
+export default Header;
