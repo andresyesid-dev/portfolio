c46c6302293f5ae095f1da5c8b68bae0e82d6eca
andresyesid-dev <andresyesid0505@gmail.com>
2024-11-26 21:36:25 -0500

    CU-86a5q4gta 86a5q4gta

---
+++ src/generals_components/Menu.tsx
@@ -0,0 +1,11 @@
+import './styles/Menu.scss'
+
+function Menu() {
+    return (
+        <section className='menu'>
+            <h1>menu</h1>
+        </section>
+    )
+}
+
+export default Menu;
---
+++ src/layout/Home.tsx
@@ -0,0 +1,10 @@
+import './styles/Home.scss'
+
+function Home() {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
+export default Home
